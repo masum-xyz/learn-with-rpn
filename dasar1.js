50312410bf5a92c2f94990d85caae37bdadb6226
masum.xyz <masumichsan92@gmail.com>
2023-12-03 19:26:49 +0700

    first push

---
+++ dasar1.js
@@ -0,0 +1,60 @@
+// var
+var a = 'a';
+let b = 'b';
+const c = 'c';
+
+// Variabel bisa menampung tipe data string (text), number (angka), boolean (true/false), array, object, function, dll
+
+let nama = 'Masum';
+let umur = 19;
+let apakahBenar = true; //boolean
+let uang = 5000.5; //number
+
+// operator aritmatika
+
+let angka1 = 10;
+let angka2 = 20;
+console.log(angka1 + angka2);
+console.log(angka1 - angka2);
+console.log(angka1 * angka2);
+console.log(angka1 / angka2);
+console.log(angka1 % angka2);
+
+// Belajar ngisi variabel
+let penampungHasilOperatorAritmatika = angka1 + angka2;
+console.log(penampungHasilOperatorAritmatika);
+let penampungHasilOperatorAritmatikaK = angka1 - angka2;
+console.log(penampungHasilOperatorAritmatikaK);
+let penampungHasilOperatorAritmatikaL = angka1 * angka2;
+console.log(penampungHasilOperatorAritmatikaL);
+let penampungHasilOperatorAritmatikaB = angka1 / angka2;
+console.log(penampungHasilOperatorAritmatikaB);
+
+// String bisa di tambah
+let firstName = 'Masum';
+let lastName = 'Ihsan';
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+console.log(firstName + ' ' + lastName);
+console.log(firstName - lastName);
+
+// Alur pembacaan kode
+let number = 20;
+console.log(number + 10);
+number = 10;
+console.log(number + 10);
+number = false;
+console.log(number + 10);
+
+// Kenapa false - 10 = 10 ? karena false diubah menjadi = 0 dan true = 1
+console.log(true + 10);
+
+// Membaca error
+// jgn takut dgn Error, justru harus takut saat programmu gada error hahaha
+
+// Contoh error
+// const hewan = 'Jerapah';
+// console.log(hewan);
+// hewan = 'Kucing';
+
+
---
+++ test.js
@@ -0,0 +1,16 @@
+// PART 2 ALGORITMA
+
+/*
+cara memasukan baju ke mesin cuci
+misal mesin cuci hanya muat 3 baju saja
+lalu kita butuh mencuci 4 baju
+
+langkah awal
+kita masukan 3 baju sekaligus ke mesin cuci tersebut
+kalau kita memasukan 4 baju sekaligus maka akan ada peringatan dari mesin cucinya
+
+lalu kalau sudah memasukan 3 baju atau kurang dari itu
+baju yang kotor dikurangi baju yang masuk ke mesin cuci tersebut
+
+lalu lakukan hal tersebut sampai baju kotornya habis 
+*/
