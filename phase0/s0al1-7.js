f6a5af6f50632b5cf463d879d967f9b9c8e17702
masum.xyz <masumichsan92@gmail.com>
2024-03-17 06:05:22 +0700

    soal ngoding minggu pertama

---
+++ phase0/s0al1-7.js
@@ -0,0 +1,100 @@
+// Problem
+// Diberikan sebuah function bandingkanAngka(angka1, angka2) yang menerima dua parameter angka.
+// Function akan me-return nilai true jika angka2 lebih besar dari angka1, dan false jika sebaliknya.
+// Jika kedua angka bernilai sama, function akan me-return -1.
+
+function bandingkanAngka(angka1, angka2) {
+	if (angka1 < angka2) {
+		return true;
+	} else if (angka1 > angka2) {
+		return false;
+	} else {
+		return -1;
+	}
+}
+
+// TEST CASES
+console.log(bandingkanAngka(5, 8)); // true
+console.log(bandingkanAngka(5, 3)); // false
+console.log(bandingkanAngka(4, 4)); // -1
+console.log(bandingkanAngka(3, 3)); // -1
+console.log(bandingkanAngka(17, 2)); // false
+
+// ------------------------------------
+console.log('');
+// ------------------------------------
+
+// Problem
+// Diberikan sebuah function balikKata(kata) yang menerima satu parameter berupa string.
+// Function akan me-return kata yang dibalik. Contoh, jika kata adalah "John Doe", function akan me-return "eoD nhoJ".
+
+function balikKata(kata) {
+	let result = '';
+	for (let i = kata.length - 1; i >= 0; i--) {
+		result += kata[i];
+	}
+	return result;
+}
+// TEST CASES
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('John Doe')); // eoD nhoJ
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(balikKata('Super')); // repuS
+
+// ------------------------------------
+console.log('');
+// ------------------------------------
+
+// Problem
+// Diberikan sebuah function konversiMenit(menit) yang menerima satu parameter berupa angka yang merupakan ukuran waktu
+// dalam menit. Function akan me-return string waktu dalam format jam:menit berdasarkan menit tersebut.
+// Contoh, jika menit adalah 63, maka function akan me-return "1:03".
+
+function konversiMenit(menit) {
+	let hours = Math.floor(menit / 60);
+	let minutes = menit % 60;
+	if (minutes < 10) {
+		minutes = '0' + minutes;
+	}
+	return `${hours}:${minutes}`;
+}
+
+// TEST CASES
+console.log(konversiMenit(63)); // 1:03
+console.log(konversiMenit(124)); // 2:04
+console.log(konversiMenit(53)); // 0:53
+console.log(konversiMenit(88)); // 1:28
+console.log(konversiMenit(120)); // 2:00
+
+// ------------------------------------
+console.log('');
+// ------------------------------------
+
+//   Problem
+//   Diberikan sebuah function xo(str) yang menerima satu parameter berupa string.
+//   Function akan me-return true jika jumlah karakter x sama dengan jumlah karakter o, dan false jika tidak.
+
+function xo(str) {
+	let x = 0;
+	let o = 0;
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] === 'x') {
+			x++;
+		} else if (str[i] === 'o') {
+			o++;
+		}
+	}
+	if (x === o) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+// TEST CASES
+console.log(xo('xoxoxo')); // true
+console.log(xo('oxooxo')); // false
+console.log(xo('oxo')); // false
+console.log(xo('xxxooo')); // true
+console.log(xo('xoxooxxo')); // true
